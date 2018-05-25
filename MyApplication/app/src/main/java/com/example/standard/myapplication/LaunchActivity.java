dd817da7209d119b2a0f362a0c0d1306e9e5aa06
Chris <Chris.P.O'Riordan@aib.ie>
2018-05-25 09:37:17 +0100

    Added Image for Launch Screen
    This is using Picasso Image management Library.

--- MyApplication/app/src/main/java/com/example/standard/myapplication/LaunchActivity.java
+++ MyApplication/app/src/main/java/com/example/standard/myapplication/LaunchActivity.java
@@ -8,7 +8,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+
+import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,21 +19,17 @@ import butterknife.ButterKnife;
 public class LaunchActivity extends AppCompatActivity {
 
     @BindView(R.id.progressBar) ProgressBar progressBar;
+    @BindView(R.id.logo) ImageView ivLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Remove title bar
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        //Remove notification bar
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         //set content view AFTER ABOVE sequence (to avoid crash)
         this.setContentView(R.layout.activity_launch);
 
         ButterKnife.bind(this);
+        Picasso.get().load(R.drawable.aib_logo).into(ivLogo);
         Loading loading = new Loading();
         loading.execute();
     }
